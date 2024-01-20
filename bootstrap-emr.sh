fed0af54b8a354c7ae0418b1570a3963b163dd04
sdav1208 <124454571+sdav1208@users.noreply.github.com>
2024-01-20 13:50:09 +0100

    Add files via upload

---
+++ bootstrap-emr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install -U wheel
+sudo python3 -m pip install -U pillow
+sudo python3 -m pip install -U pandas
+sudo python3 -m pip install pyarrow
+sudo python3 -m pip install -U boto3
+sudo python3 -m pip install -U s3fs
+sudo python3 -m pip install -U fsspec
+sudo python3 -m pip install -U keras
